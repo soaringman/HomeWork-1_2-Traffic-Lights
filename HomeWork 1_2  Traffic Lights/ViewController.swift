6bce42e3b962412c8b30714fbb8918f078cd0813
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-09-06 21:20:12 +0300

    Initial Commit

---
+++ HomeWork 1_2  Traffic Lights/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork 1_2  Traffic Lights
+//
+//  Created by Алексей Гуляев on 06.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
